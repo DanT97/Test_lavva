21deeefe5d0eca6508f785e8eb4daae169034110
Daniel Tavares <danieltavares@MBP-de-Daniel.lan>
2019-01-25 14:44:08 +0000

    F C

---
+++ index.js
@@ -0,0 +1,123 @@
+var btn = document.querySelector('#plus_btn');
+btn.addEventListener('click',method1);
+btn.addEventListener('click',method2);
+function method2(){
+  div = document.getElementById('second_cont_alternative');
+  div.style.display = "block";
+}
+function method1(){
+    div = document.getElementById('second_cont');
+    div.style.display = "none";
+}
+
+
+var btn = document.querySelector('#x_btn');
+btn.addEventListener('click',method3);
+btn.addEventListener('click',method4);
+function method3(){
+  div = document.getElementById('second_cont_alternative');
+  div.style.display = "none";
+}
+function method4(){
+    div = document.getElementById('second_cont');
+    div.style.display = "flex";
+}
+
+//****************************SliderJS
+$(document).ready(function(){
+
+  $("#slideshow > div:gt(0)").hide();
+
+  var buttons = "";
+  
+  var slidesl = $('.slideitem').length
+  var d = "<li class=\"dot active-dot\">&bull;</li>";
+  for (var i = 1; i < slidesl; i++) {
+    d = d+"<li class=\"dot\">&bull;</li>";
+  }	
+  var dots = "<ul class=\"slider-dots\">" + d + "</ul\>";
+  
+  $("#slideshow").append(dots).append(buttons);
+  var interval = setInterval(slide, 3000);
+  
+  function intslide(func) {
+    if (func == 'start') { 
+     interval = setInterval(slide, 3000);
+    } else {
+      clearInterval(interval);		
+      }
+  }
+  
+  function slide() {
+  }
+    
+  function sact(a, ix, it) {
+          var currentSlide = $('.current');
+          var nextSlide = currentSlide.next('.slideitem');
+          var prevSlide = currentSlide.prev('.slideitem');
+          var reqSlide = $('.slideitem').eq(ix);
+  
+          var currentDot = $('.active-dot');
+          var nextDot = currentDot.next();
+          var prevDot = currentDot.prev();
+          var reqDot = $('.dot').eq(ix);
+      
+          if (nextSlide.length == 0) {
+            nextDot = $('.dot').first();
+              nextSlide = $('.slideitem').first();
+              }
+  
+          if (prevSlide.length == 0) {
+            prevDot = $('.dot').last();
+              prevSlide = $('.slideitem').last();
+              }
+        
+      if (a == 'next') {
+        var Slide = nextSlide;
+        var Dot = nextDot;
+        }
+        else if (a == 'prev') {
+          var Slide = prevSlide;
+          var Dot = prevDot;
+          }
+          else {
+            var Slide = reqSlide;
+            var Dot = reqDot;
+            }
+  
+          currentSlide.fadeOut(it).removeClass('current');
+          Slide.fadeIn(it).addClass('current');
+      
+        currentDot.removeClass('active-dot');
+        Dot.addClass('active-dot');
+  }	
+  
+  $('.next').on('click', function(){
+      intslide('stop');						
+      sact('next', 0, 400);
+      intslide('start');						
+    });//next
+  
+  $('.prev').on('click', function(){
+      intslide('stop');						
+      sact('prev', 0, 400);
+      intslide('start');						
+    });//prev
+  
+  $('.dot').on('click', function(){
+      intslide('stop');
+      var index  = $(this).index();
+      sact('dot', index, 400);
+      intslide('start');						
+    });//prev
+  //slideshow
+  });
+  
+  /*
+  JQUERY SLIDER BY JohnRostislavovich - https://codepen.io/JohnRostislavovich
+  ALL YOU HAVE TO DO:
+  -copy the whole code html, css & js
+  -include jquery lib
+  -change the images
+  */
+  
